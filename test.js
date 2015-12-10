3edf8deebeae3788e3e15ce5f371508aad741f36
Jordan Harband <ljharb@gmail.com>
2015-12-10 00:36:37 -0800

    [Tests] add tests for circular references.
    Fixes #2.

--- test.js
+++ test.js
@@ -303,3 +303,20 @@ test('iterables', { skip: !symbolIterator }, function (t) {
 
 	t.end();
 });
+
+var Circular = function Circular() {
+	this.circularRef = this;
+};
+test('circular references', function (t) {
+	var a = new Circular();
+	var b = new Circular();
+	t.equal(isEqual(a, b), true, 'two circular referencing instances are equal');
+
+	var c = {};
+	var d = {};
+	c.c = c;
+	d.d = d;
+	t.equal(isEqual(c, d), false, 'two objects with different circular references are not equal');
+
+	t.end();
+});

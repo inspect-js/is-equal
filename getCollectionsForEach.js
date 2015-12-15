61c73ca97b8a1762bfcc4bc27b598ee69bda5b34
Jordan Harband <ljharb@gmail.com>
2015-12-14 22:48:35 -0800

    [Fix] ensure that https://github.com/zloirock/core-js/issues/144 doesn't cause false positives.
    Also add `npm run test:corejs`.
    
    Relates to #4.

--- getCollectionsForEach.js
+++ getCollectionsForEach.js
@@ -1,6 +1,23 @@
 module.exports = function () {
-	var mapForEach = typeof Map === 'function' ? Map.prototype.forEach : null;
-	var setForEach = typeof Set === 'function' ? Set.prototype.forEach : null;
+	var mapForEach = (function () {
+		if (typeof Map !== 'function') { return null; }
+		try {
+			Map.prototype.forEach.call({}, function () {});
+		} catch (e) {
+			return Map.prototype.forEach;
+		}
+		return null;
+	}());
+
+	var setForEach = (function () {
+		if (typeof Set !== 'function') { return null; }
+		try {
+			Set.prototype.forEach.call({}, function () {});
+		} catch (e) {
+			return Set.prototype.forEach;
+		}
+		return null;
+	}());
 
 	return { Map: mapForEach, Set: setForEach };
 };
